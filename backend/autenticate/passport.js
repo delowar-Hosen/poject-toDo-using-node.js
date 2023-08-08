3bfa7dc66fd6c3dc8c5b3972944ebd14ec3db6e9
delowar-Hosen <delowarhosen15o1121@gmail.com>
2023-08-08 10:46:45 +0600

    last

---
+++ backend/autenticate/passport.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const app = express();
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/");
+  }
+);
--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ const cors = require("cors");
 const { readdirSync } = require("fs");
 const { db } = require("./config/database");
 require("dotenv").config();
+const passport = require("passport");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const app = express();
 
 //middilware
@@ -17,6 +21,61 @@ db();
 readdirSync("./routes").map((r) => app.use("/", require("./routes/" + r)));
 
 //port
+
+//sessions
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.DATABASE_URL,
+      collectionName: "sessions",
+    }),
+    // cookie: { secure: true },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/callback",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    }
+  )
+);
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    // Redirect to the React front-end after successful authentication
+    res.redirect("http://localhost:3000");
+  }
+);
+
+app.get("/api/user", (req, res) => {
+  // Retrieve user data from the session
+  const user = req.user;
+  res.json(user);
+});
+
 const port = process.env.PORT;
 app.listen(port, () => {
   console.log("Port is running");
